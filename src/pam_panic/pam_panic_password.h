8ad9fad8e6570a743184e75bd16e121322036919
Bandie <bandie@chaospott.de>
2018-09-21 22:49:07 +0200

    gettext: pam_panic_password

--- src/pam_panic/pam_panic_password.h
+++ src/pam_panic/pam_panic_password.h
@@ -10,7 +10,6 @@ LICENSE :      GNU-GPLv3
 #ifndef PPASSFILE
   #error PPASSFILE must be declared!
 #endif
-#define PWPROMPT "Password::"
 
 int authPassword(pam_handle_t *pamh, char *serious_dev, int8_t bSerious, int8_t bReboot, int8_t bPoweroff);
 
